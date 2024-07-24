66ceb2da40162d90343f3648cc4b57578cd6c1b6
rajeshmondal54 <rm4223536@gmail.com>
2024-07-25 03:22:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function highlight() {
     //Write your code here
+	const boldElements = document.querySelectorAll('strong');
+    boldElements.forEach(element => {
+        element.style.color = 'green';
+    });
 
 
 }
@@ -7,6 +11,10 @@ function highlight() {
 
 function return_normal() {
     //Write your code here
+	const boldElements = document.querySelectorAll('strong');
+    boldElements.forEach(element => {
+        element.style.color = 'black';
+    });
 
     
 }
